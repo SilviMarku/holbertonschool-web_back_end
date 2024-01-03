957fe9d96621713f1f8016c6a9cb00780ff15134
SilviMarku <marku.silvia777@gmail.com>
2024-01-03 13:30:33 +0100

    S

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Simple module for strongly
+typed python
+'''
+
+
+def aconcat(str1: str, str2: str) -> str:
+    '''
+    function concat that takes 2 strings str1 and str2 as arguments
+    and returns a concatenated string
+    '''
+
+    return str1 + str2
