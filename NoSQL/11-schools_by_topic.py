aaba858f673ec812731626e4035179c3cf673065
SilviMarku <marku.silvia777@gmail.com>
2024-01-09 15:57:33 +0100

    S

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -5,4 +5,8 @@ NoSQL
 
 
 def schools_by_topic(mongo_collection, topic):
-    
+   '''
+   function that returns the list of school having a specific topic:
+   '''
+   value = {"topics": topic}
+   return mongo_collection.find(value)
