52021bd261c9c27b03b95bdd95b936e1896d49bc
SilviMarku <marku.silvia777@gmail.com>
2024-01-26 15:27:40 +0100

    S

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,9 @@
 function getResponseFromAPI() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, 10);
-    });
-  }
-  
-  export default getResponseFromAPI;
-  
+  return new Promise((resolve) => {
+    setTimeout(() => {
+    resolve();
+    }, 10);
+});
+}  
+
+export default getResponseFromAPI;
