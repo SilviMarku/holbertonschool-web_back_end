983d13f73d002688b7901f34600aab682f4b6dfb
SilviMarku <marku.silvia777@gmail.com>
2024-01-26 15:19:00 +0100

    Readme

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+function getResponseFromAPI() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 10);
+    });
+  }
+  
+  export default getResponseFromAPI;
+  
