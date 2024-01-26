baf2b76149d7969d8c80ad6b9578a83bcd77dd02
SilviMarku <marku.silvia777@gmail.com>
2024-01-26 15:56:13 +0100

    S

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,22 +3,9 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const premtimet = [];
-  premtimet.push(signUpUser(firstName, lastName));
-  premtimet.push(uploadPhoto(fileName));
-
-  return Promise.allSettled(premtimet)
-    .then(results => {
-      return results.map(result => ({
-        status: result.status,
-        value: (() => {
-          if (result.value === 'fulfilled') {
-            return result.value;
-          } else {
-            return result.reason;
-          }
-        })()
-      }));
-    });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
 }
