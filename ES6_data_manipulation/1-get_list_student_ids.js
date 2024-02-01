d0dce1ab402f910b226898f00af1dc14f5e7553b
SilviMarku <marku.silvia777@gmail.com>
2024-02-01 11:57:57 +0100

    R

--- ES6_data_manipulation/1-get_list_student_ids.js
+++
@@ -1,8 +0,0 @@
-/*eslint-disable*/
-export default function getListStudentIds (array) {
-    if (!Array.isArray(array)) {
-      return [];
-    }
-    return array.map((student) => student.id);
-  }
-  
